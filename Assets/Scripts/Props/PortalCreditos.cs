67a81bdf5ad396c782921d8bf250d611899ae406
Chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-02-13 00:38:17 +0100

    portal fix

---
+++ Assets/Scripts/Props/PortalCreditos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PortalCreditos : MonoBehaviour
+{
+    public PlayerController playercontroller;
+    public GameMaster gameMaster;
+    private GameObject loading;
+    public Animator tranistion;
+    public CanvasGroup hud;
+
+    //Audio
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot nopaused;
+
+    private void Start()
+    {
+        loading = GameObject.Find("loadingScreen");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Play());
+        }
+    }
+    IEnumerator Play()
+    {
+        tranistion.SetBool("PressPlay", true);
+        hud.alpha = 0;
+        loading.SetActive(true);
+        gameMaster.SavePlayerStats();
+        paused.TransitionTo(4f);
+
+        yield return new WaitForSeconds(4);
+        nopaused.TransitionTo(0.1f);
+        FindObjectOfType<AudioManager>().Stop("MenuBGM");
+        SceneManager.LoadScene("Credits");
+    }
+}
