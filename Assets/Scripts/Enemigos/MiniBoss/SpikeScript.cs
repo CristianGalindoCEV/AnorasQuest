8f4189478542fb97240208549775471913ce457c
Chaku <cristiangalindo13@gmail.com>
2021-03-17 17:45:13 +0100

    a

--- Assets/Scripts/Enemigos/MiniBoss/SpikeScript.cs
+++ Assets/Scripts/Enemigos/MiniBoss/SpikeScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SpikeScript : MonoBehaviour
 {
     private float TimeCounter = 0;
-    // Update is called once per frame
-    void Start()
+    private GameObject m_player;
+
+    private void Start()
     {
- 
+        //m_player = GameObject.Find("Player");
     }
+    private void OnEnable()
+    {
+        //transform.position = new Vector3 (m_player.transform.position.x, m_player.transform.position.y - 2, m_player.transform.position.z);
+    }
+    // Update is called once per frame
     void Update()
     {
         TimeCounter += Time.deltaTime;
