551438985bc948eda4c125afd395fc271ee25494
Chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-03-18 18:53:29 +0100

    FlyBoss ma o meno

--- Assets/Scripts/Enemigos/Fly MiniBoss/BallAttack.cs
+++ Assets/Scripts/Enemigos/Fly MiniBoss/BallAttack.cs
@@ -6,33 +6,39 @@ using DG.Tweening;
 public class BallAttack : MonoBehaviour
 {
     private GameObject m_player;
+    private GameObject m_Me;
     private Vector3 m_playerPos;
-    private float speed = 7f;
     // Start is called before the first frame update
     void Start()
     {
+        m_Me = this.gameObject;
         transform.localScale = new Vector3(0,0,0);
         m_player = GameObject.Find("Player");
         StartCoroutine(Go());
     }
 
-    private void Destroy()
-    {
-        Destroy(this.gameObject);
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            Destroy();
+            Destroy(m_Me);
         }
+    }
+
+    private void Completed()
+    {
+        StartCoroutine(Destroy());
     }
     IEnumerator Go()
     {
         transform.DOScale(2, 2f).SetEase(Ease.InSine);
         yield return new WaitForSeconds(2f);
         m_playerPos = m_player.transform.position;
-
-        transform.DOMove(m_playerPos, 1f).SetEase(Ease.InCubic).OnComplete(Destroy);
+        transform.DOMove(m_playerPos, 1f).SetEase(Ease.InCubic).OnComplete(Completed);
+    }
+    IEnumerator Destroy()
+    {
+        Destroy(m_Me);
+        yield return new WaitForSeconds(0f);
     }
 }
