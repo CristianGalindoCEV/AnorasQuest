95853e88d992efb65a28417b430e571f0fb88849
Chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-04-23 11:26:01 +0200

    FixPortals

--- Assets/Scripts/Props/PortalBoss.cs
+++ Assets/Scripts/Props/PortalBoss.cs
@@ -14,10 +14,34 @@ public class PortalBoss : MonoBehaviour
     //Audio
     public AudioMixerSnapshot paused;
     public AudioMixerSnapshot nopaused;
-
+   
+    //loading random
+    private int i_number;
+    public GameObject m_text1;
+    public GameObject m_text2;
+    public GameObject m_text3;
+    public GameObject m_image1;
+    public GameObject m_image2;
+    public GameObject m_image3;
     private void Start()
     {
         m_loading = GameObject.Find("loadingScreen");
+
+        /*
+        m_text1 = GameObject.Find("Text_Anora");
+        m_text2 = GameObject.Find("Text_Enemy");
+        m_text3 = GameObject.Find("Text_FinalBoss");
+        m_image1 = GameObject.Find("Image_Anora");
+        m_image2 = GameObject.Find("Image_Enemy");
+        m_image3 = GameObject.Find("Image_FinalBoss");
+        */
+
+        m_image1.SetActive(false);
+        m_text1.SetActive(false);
+        m_image2.SetActive(false);
+        m_text2.SetActive(false);
+        m_image3.SetActive(false);
+        m_text3.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -28,6 +52,30 @@ public class PortalBoss : MonoBehaviour
     }
     IEnumerator Play()
     {
+        i_number = Random.Range(1, 3);
+
+        switch (i_number)
+        {
+            case 1:
+                m_image1.SetActive(true);
+                m_text1.SetActive(true);
+                break;
+
+            case 2:
+                m_image2.SetActive(true);
+                m_text2.SetActive(true);
+                break;
+
+            case 3:
+                m_image3.SetActive(true);
+                m_text3.SetActive(true);
+                break;
+
+            default:
+                Debug.Log("Switch error");
+                break;
+        }
+
         tranistion.SetBool("PressPlay", true);
         hud.alpha = 0;
         m_loading.SetActive(true);
--- Assets/Scripts/Props/PortalItem.cs
+++ Assets/Scripts/Props/PortalItem.cs
@@ -16,9 +16,31 @@ public class PortalItem : MonoBehaviour
     public AudioMixerSnapshot paused;
     public AudioMixerSnapshot nopaused;
 
+    //loading random
+    private int i_number;
+    private GameObject m_text1;
+    private GameObject m_text2;
+    private GameObject m_text3;
+    private GameObject m_image1;
+    private GameObject m_image2;
+    private GameObject m_image3;
     private void Start()
     {
         loading = GameObject.Find("loadingScreen");
+
+        m_text1 = GameObject.Find("Text_Anora");
+        m_text2 = GameObject.Find("Text_Enemy");
+        m_text3 = GameObject.Find("Text_FinalBoss");
+        m_image1 = GameObject.Find("Image_Anora");
+        m_image2 = GameObject.Find("Image_Enemy");
+        m_image3 = GameObject.Find("Image_FinalBoss");
+
+        m_image1.SetActive(false);
+        m_text1.SetActive(false);
+        m_image2.SetActive(false);
+        m_text2.SetActive(false);
+        m_image3.SetActive(false);
+        m_text3.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -29,6 +51,30 @@ public class PortalItem : MonoBehaviour
     }
     IEnumerator Play()
     {
+        i_number = Random.Range(1, 3);
+
+        switch (i_number)
+        {
+            case 1:
+                m_image1.SetActive(true);
+                m_text1.SetActive(true);
+                break;
+
+            case 2:
+                m_image2.SetActive(true);
+                m_text2.SetActive(true);
+                break;
+
+            case 3:
+                m_image3.SetActive(true);
+                m_text3.SetActive(true);
+                break;
+
+            default:
+                Debug.Log("Switch error");
+                break;
+        }
+
         tranistion.SetBool("PressPlay", true);
         hud.alpha = 0;
         loading.SetActive(true);
