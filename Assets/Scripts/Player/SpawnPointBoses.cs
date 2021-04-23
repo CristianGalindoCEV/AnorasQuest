0e306de0bb725186593537681f7e3db07d4c1c13
Chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-04-23 11:51:50 +0200

    SpawnsBoses

---
+++ Assets/Scripts/Player/SpawnPointBoses.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointBoses : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStats.playerPosition_stat = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
