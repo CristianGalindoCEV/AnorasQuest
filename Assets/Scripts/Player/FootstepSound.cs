f0977ba5a0d301511474e26c9a2c565db14df0d8
PolDomingo2C <poldomingo_bcnvj1ma1920@cev.com>
2021-05-04 19:13:26 +0200

    FootstepsSmokeVFX

--- Assets/Scripts/Player/FootstepSound.cs
+++ Assets/Scripts/Player/FootstepSound.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class FootstepSound : MonoBehaviour
 {
+    VisualEffect vfxSmoke;
+    void Start()
+    {
+        vfxSmoke = GetComponent<VisualEffect>();
+    }
 
-
-    /* private void OnTriggerEnter(Collider other)
-     {
-         FindObjectOfType<AudioManager>().PlayRandomPitch("Footstep");
-         Debug.Log(":)");
-     } */
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
             FindObjectOfType<AudioManager>().PlayRandomPitch("Footstep");
-            
+            vfxSmoke.SendEvent("FootstepPlay");
         }
     }
 }
