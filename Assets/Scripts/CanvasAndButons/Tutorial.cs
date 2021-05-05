c73093c9c6092cac9de01e4eb78d9e0925340008
chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-05-05 17:21:27 +0200

    Animation Tutorial UI

--- Assets/Scripts/CanvasAndButons/Tutorial.cs
+++ Assets/Scripts/CanvasAndButons/Tutorial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
 
@@ -11,6 +12,10 @@ public class Tutorial : MonoBehaviour
     public TextMeshProUGUI JumpTutorial;
     public TextMeshProUGUI ShotTutorial;
 
+    public Image Icon_L;
+    public Image Icon_R;
+
+
     private bool b_move = false;
     private bool b_jump = false;
     private bool b_shot = false;
@@ -18,14 +23,18 @@ public class Tutorial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //UI
+        Icon_L.enabled = false;
+        Icon_R.enabled = false;
+        
+        //Tutorial
         ControlTutorial.enabled = false;
         JumpTutorial.enabled = false;
         ShotTutorial.enabled = false;
 
         if (playerStats.tutorial == false)
         {
-            ControlTutorial.enabled = true;
-            ControlTutorial.rectTransform.DOMoveY(220, 1f).SetEase(Ease.InSine);
+            StartCoroutine(MoveTutorial());
         }
         if (playerStats.tutorial == true)
         {
@@ -39,6 +48,7 @@ public class Tutorial : MonoBehaviour
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
             {
                 StartCoroutine(MyJumpTutorial());
+                StartTurotailShot();
             }
         }
         if(b_move == true && b_jump == false) //JumpTutorial
@@ -62,24 +72,91 @@ public class Tutorial : MonoBehaviour
         b_move = true;
     }
 
+    IEnumerator MoveTutorial()
+    {
+        //Reset Positions
+        ControlTutorial.enabled = true;
+        Icon_L.enabled = false;
+        Icon_R.enabled = false;
+
+        Icon_L.rectTransform.position = new Vector3(910, 150, 0);
+        Icon_R.rectTransform.position = new Vector3(910, 150, 0);
+
+        yield return new WaitForSeconds(0.1f);
+
+        //Start
+
+        Icon_L.enabled = true;
+        Icon_R.enabled = true;
+
+        Icon_L.transform.DOMoveX(450, 2f).SetEase(Ease.OutQuint);
+        Icon_R.transform.DOMoveX(1350, 2f).SetEase(Ease.OutQuint);
+        
+        yield return new WaitForSeconds(0.5f);
+        
+        ControlTutorial.DOFade(1f, 1f).SetEase(Ease.OutQuint);
+
+    }
+
     IEnumerator MyJumpTutorial()
     {
-        yield return new WaitForSeconds(1f);
-        ControlTutorial.enabled = false;
+        //Reset Positions
+        
+        Icon_L.enabled = false;
+        Icon_R.enabled = false;
+
+        Icon_L.rectTransform.position = new Vector3(910, 150, 0);
+        Icon_R.rectTransform.position = new Vector3(910, 150, 0);
+
+        yield return new WaitForSeconds(0.1f);
+        
+        //Start
+
+        Icon_L.enabled = true;
+        Icon_R.enabled = true;
         JumpTutorial.enabled = true;
-        JumpTutorial.rectTransform.DOMoveY(220, 1f).SetEase(Ease.InSine).OnComplete(StartTurotailShot);
+        ControlTutorial.enabled = false;
+
+        Icon_L.transform.DOMoveX(450, 2f).SetEase(Ease.OutQuint);
+        Icon_R.transform.DOMoveX(1350, 2f).SetEase(Ease.OutQuint);
+
+        yield return new WaitForSeconds(0.5f);
+
+        JumpTutorial.DOFade(1f, 1f).SetEase(Ease.OutQuint);
     }
     IEnumerator MyShotTutorial()
     {
-        yield return new WaitForSeconds(1f);
-        JumpTutorial.enabled = false;
+        //Reset Positions
+        
+        Icon_L.enabled = false;
+        Icon_R.enabled = false;
+        
+        Icon_L.rectTransform.position = new Vector3 (910,150,0);
+        Icon_R.rectTransform.position = new Vector3(910, 150, 0);
+
+        yield return new WaitForSeconds(0.1f);
+        
+        //Start
+
+        Icon_L.enabled = true;
+        Icon_R.enabled = true;
         ShotTutorial.enabled = true;
-        ShotTutorial.rectTransform.DOMoveY(220, 1f).SetEase(Ease.InSine);
+        JumpTutorial.enabled = false;
+
+        Icon_L.transform.DOMoveX(450, 2f).SetEase(Ease.OutQuint);
+        Icon_R.transform.DOMoveX(1350, 2f).SetEase(Ease.OutQuint);
+
+        yield return new WaitForSeconds(0.5f);
+
+        ShotTutorial.DOFade(1f, 1f).SetEase(Ease.OutQuint);
         b_jump = true;
     }
     IEnumerator MyEndTutorial()
     {
+
         yield return new WaitForSeconds(1f);
+        Icon_L.enabled = false;
+        Icon_R.enabled = false;
         ShotTutorial.enabled = false;
         playerStats.tutorial = true;
     }
