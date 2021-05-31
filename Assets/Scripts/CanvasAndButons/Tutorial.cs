d8084fe8ef57e6e00fd2727772c0cb8d351e465a
chaku <cristiangalindo_bcnvj1ma1920@cev.com>
2021-05-31 18:27:55 +0200

    Andamos preparando la intro animacion en el big level

--- Assets/Scripts/CanvasAndButons/Tutorial.cs
+++ Assets/Scripts/CanvasAndButons/Tutorial.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
+using Cinemachine;
 
 public class Tutorial : MonoBehaviour
 {
     public PlayerStats playerStats;
     public MenuManager menuManager;
+    public InputManager inputManager;
 
+    //Animation
+    private bool b_animation = false;
+    public CinemachineVirtualCamera firtsCamAnimation; // Firts cam animated intro
+    public CinemachineVirtualCamera finalCamAnimation; // Last cam animated intro
+    public PlayerController playerController;
+    [SerializeField] private float f_animationTime;
+
+    //Tutorial
     private bool b_start = false;
     private bool b_move = false;
     private bool b_jump = false;
@@ -18,46 +28,76 @@ public class Tutorial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (playerStats.tutorial == false)
+        if (playerStats.tutorial == false) // Tutorial no complete
         {
-            StartCoroutine(MoveTutorial());
+            firtsCamAnimation.enabled = true; //Animation Camera
+            playerController.speed = 0f; // Fix because can press inputs to move
+            
+            //Eneable cameras
+            playerController.aimCamera.enabled = false;
+            playerController.mainCamera.enabled = false;
+            
+            StartCoroutine(StartAnimation());
         }
+        
         if (playerStats.tutorial == true)
         {
+            //Hacer que se paaguen las camaras de animation
+            firtsCamAnimation.enabled = false;
+            b_animation = true;
             this.enabled = false;
         }
     }
     private void Update()
     {
-        if(b_move == false && b_start == true) //MoveTutorial
+        if (b_animation == true)
         {
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+            if (b_move == false && b_start == true) //MoveTutorial
             {
-                StartCoroutine(MyJumpTutorial());
-                StartTurotailShot();
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+                {
+                    StartCoroutine(MyJumpTutorial());
+                    StartTurotailShot();
+                }
             }
-        }
-        if(b_move == true && b_jump == false) //JumpTutorial
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (b_move == true && b_jump == false) //JumpTutorial
             {
-                StartCoroutine(MyShotTutorial());
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StartCoroutine(MyShotTutorial());
+                }
             }
-        }
-        if(b_jump == true && b_shot == false) //ShotTutorial
-        {
-            if (Input.GetButtonDown("Fire2"))
+            if (b_jump == true && b_shot == false) //ShotTutorial
             {
-                StartCoroutine(MyEndTutorial());
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    StartCoroutine(MyEndTutorial());
+                }
             }
         }
     }
 
+    //Animation Voids
+    IEnumerator StartAnimation()
+    {
+        inputManager.animationPlayed = true; //Cant aim
+
+        yield return new WaitForSeconds(f_animationTime);
+        StartCoroutine(MoveTutorial());
+
+        finalCamAnimation.enabled = false;
+        inputManager.animationPlayed = false; // Can Aim
+        b_animation = true;
+        firtsCamAnimation.enabled = false;
+        playerController.aimCamera.enabled = true;
+        playerController.mainCamera.enabled = true;
+    }
+
+    //Tutorial Voids
     private void StartTurotailShot()
     {
         b_move = true;
     }
-
     IEnumerator MoveTutorial()
     {
         //yield return new WaitForSeconds(1.5f);
@@ -68,7 +108,6 @@ public class Tutorial : MonoBehaviour
 
         b_start = true;
     }
-
     IEnumerator MyJumpTutorial()
     {
         yield return new WaitForSeconds(1.5f);
