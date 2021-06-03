f6cd5129a483689a5cd7b9cb9aa51e929600fb9a
PolDomingo2C <poldomingo_bcnvj1ma1920@cev.com>
2021-06-03 18:50:28 +0200

    INTRO 1 2 3 con sonido

--- Assets/Scripts/CanvasAndButons/PassScenesAnimations.cs
+++ Assets/Scripts/CanvasAndButons/PassScenesAnimations.cs
@@ -45,7 +45,7 @@ public class PassScenesAnimations : MonoBehaviour
         }
         if (Animation_3 == true)
         {
-            SceneManager.LoadScene("BIg Level");
+            SceneManager.LoadScene("BIgLevel");
         }
     }
 }
